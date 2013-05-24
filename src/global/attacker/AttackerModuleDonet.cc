35f1612b31544d16dff66353b4eb0e22e7061266
Giang Nguyen <nguyen@cs.tu-darmstadt.de>
2013-05-24 17:12:19 +0200

    added AttackerModuleDonet.{ned,h,cc} which inherit AttackerModule

---
+++ src/global/attacker/AttackerModuleDonet.cc
@@ -0,0 +1,157 @@
+//
+// =============================================================================
+// OSSIM : A Generic Simulation Framework for Overlay Streaming
+// =============================================================================
+//
+// (C) Copyright 2012-2013, by Giang Nguyen (P2P, TU Darmstadt) and Contributors
+//
+// Project Info: http://www.p2p.tu-darmstadt.de/research/ossim
+//
+// OSSIM is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// OSSIM is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <http://www.gnu.org/licenses/>.
+
+// -----------------------------------------------------------------------------
+// AttackerModuleDonet.cc
+// -----------------------------------------------------------------------------
+// (C) Copyright 2012-2013, by Giang Nguyen (P2P, TU Darmstadt) and Contributors
+//
+// Contributors: Mathias Fischer;
+// Code Reviewers: Giang Nguyen;
+// -----------------------------------------------------------------------------
+//
+
+#include "AttackerModuleDonet.h"
+//#include "OverlayTopology.h"
+//#include "TopologyModel.h"
+
+using namespace std;
+
+Define_Module(AttackerModuleDonet);
+
+/**
+ * initialise the AttackerModule in Stage 3.
+ */
+void AttackerModuleDonet::initialize(int stage)
+{
+
+//   if (stage != 3) return;
+
+//   //FIXME get pointer to OverlayTopology-module
+//   oT = NULL;
+
+//   // the global attacker parameters
+//   startAttack = par("start");
+//   stopAttack = par("stop");
+//   assert(startAttack < stopAttack);
+//   interval = par("interval");
+
+//   // Percentage of nodes to attack
+//   percentage = par("percentage");
+//   numAttack = par("numAttack");
+
+//   // the statistical parameters
+//   statname_damage_global = "streaming_importance_global";
+
+//   // init statistics
+//   initStatistics();
+
+//   // schedule attacks+statistics
+//   scheduleAt(simTime() + startAttack, new cMessage("GLOBAL_SCHED_ID"));
+//   updateDisplay();
+
+}
+
+//TopologyModel AttackerModule::getTopo(const int sequence) {
+
+//   return oT->getTopology(sequence);
+//}
+
+
+void AttackerModuleDonet::finish() {
+
+//   // *** Statistics ***
+//   char buffer[128];
+//   // - write damage to sca
+//   sprintf(buffer, "%s - %s", "attacker.damage", "mean");
+//   recordScalar(buffer, attackerDamage.getMean());
+//   sprintf(buffer, "%s - %s", "attacker.damage", "min");
+//   recordScalar(buffer, attackerDamage.getMin());
+//   sprintf(buffer, "%s - %s", "attacker.damage", "max");
+//   recordScalar(buffer, attackerDamage.getMax());
+}
+
+void AttackerModuleDonet::handleMessage(cMessage* msg)
+{
+//   if (!msg->isSelfMessage()) {
+//      delete msg;
+//      msg = NULL;
+//      throw cRuntimeError("AttackerModule cannot handle non-loopback messages");
+//   }
+
+//   attackGlobal();
+
+//   if (simTime() + interval <= stopAttack) {
+//      scheduleAt(simTime() + interval, msg);
+//      msg = NULL;
+//      return;
+
+//   } else {
+//      delete msg;
+//      msg = NULL;
+//   }
+
+//   assert(msg == NULL);
+}
+
+
+//void AttackerModule::attackGlobal() {
+
+   // FIXME: attackRecursive is called from oT or from the attacker itself?
+   // put stats here
+   //int damage = oT->attackRecursive(numAttack);
+
+   // FIXME - Giang: cannot cast from damage into double
+   //FIXME recording the damage
+//   attackerDamage.collect((double) damage);
+//   attackerDamageTime.record((double) damage);
+//}
+
+
+/**
+ * starts statistics
+ */
+//void AttackerModule::initStatistics() {
+//   nodesOverallVec.setName("total_number_nodes");
+//}
+
+/**
+ * omnetpp standard method
+ *
+ * There are two kinds of messages to come in:
+ *   - loopback: ATTACK_MSG_ID: triggers the global attacker
+ *   - loopback: AGENT_MSG_ID: triggers the attack agent
+ */
+//void AttackerModule::updateDisplay() {
+//#if _DEBUG
+//   char buffer[32];
+//   sprintf(buffer, "%d getNode(s)", (int)modules.size());
+
+//   cDisplayString display = getDisplayString();
+//   // to show the current amount of nodes
+//   display.setTagArg("t", 0, buffer);
+//   display.setTagArg("t", 1, "t");
+//   display.setTagArg("t", 2, "darkgray");
+//   // and also the online status
+//   setDisplayString(display.str());
+//#endif
+//}
+
---
+++ src/global/attacker/AttackerModuleDonet.h
@@ -0,0 +1,86 @@
+//
+// =============================================================================
+// OSSIM : A Generic Simulation Framework for Overlay Streaming
+// =============================================================================
+//
+// (C) Copyright 2012-2013, by Giang Nguyen (P2P, TU Darmstadt) and Contributors
+//
+// Project Info: http://www.p2p.tu-darmstadt.de/research/ossim
+//
+// OSSIM is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// OSSIM is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <http://www.gnu.org/licenses/>.
+
+// -----------------------------------------------------------------------------
+// AttackerModuleDonet.h
+// -----------------------------------------------------------------------------
+// (C) Copyright 2012-2013, by Giang Nguyen (P2P, TU Darmstadt) and Contributors
+//
+// Contributors: Mathias Fischer;
+// Code Reviewers: Giang Nguyen;
+// -----------------------------------------------------------------------------
+//
+
+#ifndef ATTACKERMODULEDONET_H
+#define ATTACKERMODULEDONET_H
+
+#include <string>
+#include <omnetpp.h>
+#include "AttackerModule.h"
+
+class OverlayTopology;
+class TopologyModel;
+
+class AttackerModuleDonet : public AttackerModule
+{
+//protected:
+
+   // *************** general purpose ***************
+//   void initStatistics();
+//   void updateDisplay();
+
+//public:
+protected:
+
+   // *************** OMNETPP+INET *****************
+   void initialize(int stage);
+   virtual int numInitStages() const {	return 5; }
+   void finish();
+   void handleMessage(cMessage* msg);
+
+   // **************** topology change *************
+//   TopologyModel getTopo(const int sequence);
+
+   // ****** Attacker ****
+//   void attackGlobal();
+//   int attackRecursive(const int sequence, const int num);
+
+protected:
+
+   // ********** general configuration ***********
+//   double rejoin;
+//   std::string damage;
+//   std::string estimate;
+
+   // ********** global attacker ****************
+//   double startAttack, stopAttack, interval, percentage;
+//   OverlayTopology* oT;
+//   int numAttack;
+
+   // ************** statistics ****************
+//   std::string statname_damage_global;
+//   cStdDev attackerDamage;
+//   cOutVector attackerDamageTime;
+//   cOutVector nodesOverallVec;
+
+};
+
+#endif // ATTACKERMODULEDONET_H
