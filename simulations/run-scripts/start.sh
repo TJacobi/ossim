6ae44492f0ccc240f543752224767bc1e75ed9d9
Giang Nguyen <nguyen@cs.tu-darmstadt.de>
2013-05-31 18:13:23 +0200

    a running start.sh script file

--- simulations/run-scripts/start.sh
+++ simulations/run-scripts/start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-opp_runall -j1 ../../src/so -r 0 $1 
+# Assuming that the file is executed within the run-script/ folder
+
+# $1 = Configuration name
+# $2 = INI file which stores the configuration
+
+#$1=Donet_oneRouter_overlayAttack 
+#$2=../Donet-overlayAttack.ini
+
+opp_runall -j1 ../../src/so -r 0 -u Cmdenv -c $1 $2 -n ../../src:..:../../../inet/src
